216b686f519ae2fc8db652db4bb16807810b6f2a
RaduTek <radutek13@gmail.com>
2023-06-29 17:05:12 +0300

    Update example for modals

--- js/script.js
+++ js/script.js
@@ -4,3 +4,15 @@ function onload() {
     console.clear();
     pageNav1 = new PageNavigator("screen1");
 }
+
+function modalpopup() {
+    MessagePopup.Show(
+        "Sample text string is here",
+        "Sample title",
+        "OK",
+        "2nd button",
+        (button) => {
+            console.log("Button pressed: " + (button ? "1" : "2"));
+        }
+    );
+}
