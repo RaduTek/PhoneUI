b212e51c189b563e0c973f8179af3882321964b9
RaduTek <radutek13@gmail.com>
2023-07-15 08:29:00 +0300

    Update demo sample

--- js/script.js
+++ js/script.js
@@ -5,14 +5,25 @@ function onload() {
     pageNav1 = new PageNavigator("screen1");
 }
 
-function modalpopup() {
-    MessagePopup.Show(
-        "Sample text string is here",
-        "Sample title",
-        "OK",
-        "2nd button",
+function modalpopup(twoButton) {
+    PopupModal.ShowMessage(
+        "This is a popup message modal. Look in the debug console for more.",
+        "Popup Modal",
         (button) => {
-            console.log("Button pressed: " + (button ? "1" : "2"));
+            console.log(`${button ? "1st" : "2nd"} button was pressed.`);
+        },
+        "OK",
+        twoButton ? "2nd button" : null
+    );
+}
+
+function modaltext() {
+    AttachedModal.ShowTextPrompt(
+        "Enter a string below. It will be printed in the debug console.",
+        "Text Prompt",
+        (confirmed, text) => {
+            if (confirmed) console.log(`Text prompt was confirmed with string: \"${text}\"`);
+            else console.log(`Text prompt was cancelled with string: \"${text}\"`);
         }
     );
 }
